0776f603f7b47760caa29547eebc333253e5cda2
Arthur Caumartin <artcaumart@gmail.com>
2023-12-16 15:41:47 +0100

    webGL fix

--- Assets/Scripts/View/BoxControler.cs
+++ Assets/Scripts/View/BoxControler.cs
@@ -176,7 +176,7 @@ public class BoxControler : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     {
         _isRunningIdleAnimation = false;
         Vector2 startPosition = _rectTransform.localPosition;
-        float pixelHeight = Camera.main.pixelHeight / 2;
+        float pixelHeight = Camera.main.pixelHeight;
         Vector2 endPosition = Random.insideUnitCircle.normalized * (pixelHeight + (pixelHeight * 0.1f));
         DOTween.To((time) =>
         {
